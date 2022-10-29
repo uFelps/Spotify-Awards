8931f69b7a62151aecbb1e5831ce4ef6a5f5b51f
Fulano de Tal <felipesand824@gmail.com>
2022-10-29 00:43:11 -0300

    Project Created

---
+++ backend/src/main/java/com/felps/Spotify/Awards/SpotifyAwardsApplication.java
@@ -0,0 +1,13 @@
+package com.felps.Spotify.Awards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyAwardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyAwardsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/felps/Spotify/Awards/SpotifyAwardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felps.Spotify.Awards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyAwardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
