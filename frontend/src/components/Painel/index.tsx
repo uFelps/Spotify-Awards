d7c2c6f41f03ecdcc5eabe0bf91c6877a33a84cf
Fulano de Tal <felipesand824@gmail.com>
2022-11-12 21:48:58 -0300

    Painel - Section 1

--- frontend/src/components/Painel/index.tsx
+++ frontend/src/components/Painel/index.tsx
@@ -1,10 +1,23 @@
-function Painel(){
-    return (
-        <>
-        <h1>Main</h1>
-        </>
-    );
+import Img from "assets/img/celular.png";
+import "./style.css";
+
+function Painel() {
+  return (
+    <>
+      <div className="background">
+        <div className="box">
+          <div className="text">
+            <p>Spotify Presents</p>
+            <h1>Spotify Awards</h1>
+            <h4>Avalie os principais artistas, albuns e musicas do ano.</h4>
+          </div>
+          <div className="img-celular">
+            <img src={Img} alt="Celular Spotify" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Painel;
-
